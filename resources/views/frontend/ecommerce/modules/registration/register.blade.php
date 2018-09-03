3763580ad3dc3c5b7fa3c8527f40415ded896d83
itfalcons <hassanzaidi20@gmail.com>
2018-09-03 17:20:33 +0500

    register change
    change register

--- resources/views/frontend/ecommerce/modules/registration/register.blade.php
+++ resources/views/frontend/ecommerce/modules/registration/register.blade.php
@@ -7,26 +7,26 @@
     <title>Bushido - HTML5 E-Commerce Template</title>
     <!--SEO Meta Tags-->
     <meta name="description" content="Responsive HTML5 E-Commerce Template" />
-		<meta name="keywords" content="responsive html5 template, e-commerce, shop, bootstrap 3.0, css, jquery, flat, modern" />
-		<meta name="author" content="8Guild" />
+    <meta name="keywords" content="responsive html5 template, e-commerce, shop, bootstrap 3.0, css, jquery, flat, modern" />
+    <meta name="author" content="8Guild" />
     <!--Mobile Specific Meta Tag-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <!--Favicon-->
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <!--Master Slider Styles-->
-    <link href="{{URL::to('public/frontend/ecommerce/assets')}}/masterslider/style/masterslider.css" rel="stylesheet" media="screen">
+    <link href="{{url::to('public/Ecommerce_Frontend/assets')}}/masterslider/style/masterslider.css" rel="stylesheet" media="screen">
     <!--Styles-->
-    <link href="{{URL::to('public/frontend/ecommerce/assets')}}/css/styles.css" rel="stylesheet" media="screen">
+    <link href="{{url::to('public/Ecommerce_Frontend/assets')}}/css/styles.css" rel="stylesheet" media="screen">
     <!--Color Scheme-->
-    <link class="color-scheme" href="{{URL::to('public/frontend/ecommerce/assets')}}/css/colors/color-default.css" rel="stylesheet" media="screen">
+    <link class="color-scheme" href="{{url::to('public/Ecommerce_Frontend/assets')}}/css/colors/color-default.css" rel="stylesheet" media="screen">
     <!--Color Switcher-->
-    <link href="{{URL::to('public/frontend/ecommerce/assets')}}/color-switcher/color-switcher.css" rel="stylesheet" media="screen">
+    <link href="{{url::to('public/Ecommerce_Frontend/assets')}}/color-switcher/color-switcher.css" rel="stylesheet" media="screen">
     <!--Modernizr-->
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/libs/modernizr.custom.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/libs/modernizr.custom.js"></script>
     <!--Adding Media Queries Support for IE8-->
     <!--[if lt IE 9]>
-      <script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/respond.js"></script>
+      <script src="js/plugins/respond.js"></script>
     <![endif]-->
  
   </head>
@@ -34,50 +34,50 @@
   <!--Body-->
   <body>
   
-  	<!--Color Switcher-->
+    <!--Color Switcher-->
     <div class="color-switcher group animated">
-    	<div class="toggle"><i class="fa fa-cog"></i></div>
-    	<div class="color">
-      	<a class="current" style="background-image:url(color-switcher/img/default.png);" href="#" data-color="default"></a>
+      <div class="toggle"><i class="fa fa-cog"></i></div>
+      <div class="color">
+        <a class="current" style="background-image:url(color-switcher/img/default.png);" href="#" data-color="default"></a>
         <span>#2ba8db<br/>#a3c756</span>
       </div>
-    	<div class="color">
-      	<a style="background-image:url(color-switcher/img/scheme2.png);" href="#" data-color="scheme2"></a>
+      <div class="color">
+        <a style="background-image:url(color-switcher/img/scheme2.png);" href="#" data-color="scheme2"></a>
         <span>#a1b0ca<br/>#e7807f</span>
       </div>
-    	<div class="color">
-      	<a style="background-image:url(color-switcher/img/scheme3.png);" href="#" data-color="scheme3"></a>
+      <div class="color">
+        <a style="background-image:url(color-switcher/img/scheme3.png);" href="#" data-color="scheme3"></a>
         <span>#4bb5c1<br/>#bfa995</span>
       </div>
-    	<div class="color">
-      	<a style="background-image:url(color-switcher/img/scheme4.png);" href="#" data-color="scheme4"></a>
+      <div class="color">
+        <a style="background-image:url(color-switcher/img/scheme4.png);" href="#" data-color="scheme4"></a>
         <span>#73cbbe<br/>#c7b07b</span>
       </div>
     </div><!--Color Switcher End-->
   
-  	<!--Login Modal-->
+    <!--Login Modal-->
     <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i></button>
-            <h2>Login or <a href="{{ route('register') }}">Register</a></h2>
+            <h2>Login or <a href="register.html">Register</a></h2>
             <p class="large">Use social accounts</p>
             <div class="social-login">
-            	<a class="facebook" href="#"><i class="fa fa-facebook-square"></i></a>
-            	<a class="google" href="#"><i class="fa fa-google-plus-square"></i></a>
-            	<a class="twitter" href="#"><i class="fa fa-twitter-square"></i></a>
+              <a class="facebook" href="#"><i class="fa fa-facebook-square"></i></a>
+              <a class="google" href="#"><i class="fa fa-google-plus-square"></i></a>
+              <a class="twitter" href="#"><i class="fa fa-twitter-square"></i></a>
             </div>
           </div>
           <div class="modal-body">
           <form class="login-form">
             <div class="form-group group">
-            	<label for="log-email">Email</label>
+              <label for="log-email">Email</label>
               <input type="email" class="form-control" name="log-email" id="log-email" placeholder="Enter your email" required>
               <a class="help-link" href="#">Forgot email?</a>
             </div>
             <div class="form-group group">
-            	<label for="log-password">Password</label>
+              <label for="log-password">Password</label>
               <input type="text" class="form-control" name="log-password" id="log-password" placeholder="Enter your password" required>
               <a class="help-link" href="#">Forgot password?</a>
             </div>
@@ -91,10 +91,7 @@
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
 
-    <!--Header-->
-   @include('frontend/ecommerce/include/header') 
-    <!--Header Close-->
-    
+    @include('frontend/include/header')
     <!--Page Content-->
     <div class="page-content">
     
@@ -113,9 +110,9 @@
         <a class="google" href="#"><i class="fa fa-google-plus-square"></i></a>
         <a class="twitter" href="#"><i class="fa fa-twitter-square"></i></a>
         </div>
-      	<div class="row">
-        	<!--Login-->
-        	<div class="col-lg-5 col-md-5 col-sm-5">
+        <div class="row">
+          <!--Login-->
+          <div class="col-lg-5 col-md-5 col-sm-5">
             <form method="post" class="login-form">
               <div class="form-group group">
                 <label for="log-email2">Email</label>
@@ -136,6 +133,18 @@
           <!--Registration-->
           <div class="col-lg-7 col-md-7 col-sm-7">
             <form method="post" class="registr-form">
+
+                     <div class="form-group group">
+                <label for="log-fname">First_Name</label>
+                <input type="text" class="form-control" name="log-fname" id="log-fname" placeholder="Enter your First_Name" required>
+              </div>
+
+              <div class="form-group group">
+                <label for="log-lname">Last_Name</label>
+                <input type="text" class="form-control" name="log-lname" id="log-lname" placeholder="Enter your Last_Name" required>
+              </div>
+
+
               <div class="form-group group">
                 <label for="rf-email">Email</label>
                 <input type="email" class="form-control" name="rf-email" id="rf-email" placeholder="Enter email" required>
@@ -159,15 +168,15 @@
       
       <!--Brands Carousel Widget-->
       <section class="brand-carousel">
-      	<div class="container">
-        	<h2>Brands in our shop</h2>
+        <div class="container">
+          <h2>Brands in our shop</h2>
           <div class="inner">
-          	<a class="item" href="#"><img src="img/brands/jquery.png" alt="jQuery"/></a>
-            <a class="item" href="#"><img src="img/brands/wp.png" alt="WordPress"/></a>
-            <a class="item" href="#"><img src="img/brands/woo.png" alt="WooCommerce"/></a>
-            <a class="item" href="#"><img src="img/brands/android.png" alt="Android"/></a>
-            <a class="item" href="#"><img src="img/brands/macgasm.png" alt="Macgasm"/></a>
-            <a class="item" href="#"><img src="img/brands/dropbox.png" alt="Dropbox"/></a>
+            <a class="item" href="#"><img src="{{url::to('public/Ecommerce_Frontend/assets')}}/img/brands/jquery.png" alt="jQuery"/></a>
+            <a class="item" href="#"><img src="{{url::to('public/Ecommerce_Frontend/assets')}}/img/brands/wp.png" alt="WordPress"/></a>
+            <a class="item" href="#"><img src="{{url::to('public/Ecommerce_Frontend/assets')}}/img/brands/woo.png" alt="WooCommerce"/></a>
+            <a class="item" href="#"><img src="{{url::to('public/Ecommerce_Frontend/assets')}}/img/brands/android.png" alt="Android"/></a>
+            <a class="item" href="#"><img src="{{url::to('public/Ecommerce_Frontend/assets')}}/img/brands/macgasm.png" alt="Macgasm"/></a>
+            <a class="item" href="#"><img src="{{url::to('public/Ecommerce_Frontend/assets')}}/img/brands/dropbox.png" alt="Dropbox"/></a>
           </div>
         </div>
       </section><!--Brands Carousel Close-->
@@ -176,19 +185,19 @@
     
     <!--Sticky Buttons-->
     <div class="sticky-btns">
-    	<form class="quick-contact ajax-form" method="post" name="quick-contact">
-      	<h3>Contact us</h3>
+      <form class="quick-contact ajax-form" method="post" name="quick-contact">
+        <h3>Contact us</h3>
         <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do.</p>
         <div class="form-group">
-        	<label for="qc-name">Full name</label>
+          <label for="qc-name">Full name</label>
           <input class="form-control input-sm" type="text" name="name" id="qc-name" placeholder="Enter full name">
         </div>
         <div class="form-group">
-        	<label for="qc-email">Email</label>
+          <label for="qc-email">Email</label>
           <input class="form-control input-sm" type="email" name="email" id="qc-email" placeholder="Enter email">
         </div>
         <div class="form-group">
-        	<label for="qc-message">Your message</label>
+          <label for="qc-message">Your message</label>
           <textarea class="form-control input-sm" name="message" id="qc-message" placeholder="Enter your message"></textarea>
         </div>
         <!-- Validation Response -->
@@ -196,7 +205,7 @@
         <!-- Response End -->
         <input class="btn btn-success btn-sm btn-block" type="submit" value="Send">
       </form>
-    	<span id="qcf-btn"><i class="fa fa-envelope"></i></span>
+      <span id="qcf-btn"><i class="fa fa-envelope"></i></span>
       <span id="scrollTop-btn"><i class="fa fa-chevron-up"></i></span>
     </div><!--Sticky Buttons Close-->
     
@@ -232,29 +241,26 @@
       </div>
     </section><!--Subscription Widget Close-->
       
-  	<!--Footer-->
-    @include('frontend/ecommerce/include/footer')
-    <!--Footer Close-->
-    
+    @include('frontend/include/footer')
     <!--Javascript (jQuery) Libraries and Plugins-->
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/libs/jquery-1.11.1.min.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/libs/jquery-ui-1.10.4.custom.min.js"></script>
-    <script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/libs/jquery.easing.min.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/bootstrap.min.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/smoothscroll.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/jquery.validate.min.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/icheck.min.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/jquery.placeholder.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/jquery.stellar.min.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/jquery.touchSwipe.min.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/jquery.shuffle.min.js"></script>
-    <script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/lightGallery.min.js"></script>
-    <script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/owl.carousel.min.js"></script>
-    <script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/masterslider.min.js"></script>
-    <script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/plugins/jquery.nouislider.min.js"></script>
-    <script src="mailer/mailer.js"></script>
-		<script src="{{URL::to('public/frontend/ecommerce/assets')}}/js/scripts.js"></script>
-    <script src="{{URL::to('public/frontend/ecommerce/assets')}}/color-switcher/color-switcher.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/libs/jquery-1.11.1.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/libs/jquery-ui-1.10.4.custom.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/libs/jquery.easing.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/bootstrap.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/smoothscroll.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/jquery.validate.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/icheck.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/jquery.placeholder.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/jquery.stellar.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/jquery.touchSwipe.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/jquery.shuffle.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/lightGallery.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/owl.carousel.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/masterslider.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/plugins/jquery.nouislider.min.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/mailer/mailer.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/js/scripts.js"></script>
+    <script src="{{url::to('public/Ecommerce_Frontend/assets')}}/color-switcher/color-switcher.js"></script>
 
     
   </body><!--Body Close-->
