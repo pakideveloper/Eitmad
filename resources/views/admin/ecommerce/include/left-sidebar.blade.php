c2d5cbbd01a4c99035c303aaa4c655bc0077b033
pakideveloper <ilovepakistan811@gmail.com>
2018-09-05 00:11:00 -0700

    set admin template

--- resources/views/admin/ecommerce/include/left-sidebar.blade.php
+++ resources/views/admin/ecommerce/include/left-sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="left side-menu">
+ <div class="left side-menu">
                 <div class="sidebar-inner slimscrollleft">
 
                     <!--- Sidemenu -->
--- resources/views/admin/ecommerce/modules/index.blade.php
+++ resources/views/admin/ecommerce/modules/index.blade.php
@@ -14,7 +14,7 @@
         <title>Zircos - Responsive Admin Dashboard Template</title>
 
         <!--Morris Chart CSS -->
-        <link rel="stylesheet" href="{{URL::to('public/admin/ecommerce')}}/plugins/morris/morris.css">
+        <link rel="stylesheet" href="../plugins/morris/morris.css">
 
         <!-- App css -->
         <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
@@ -24,7 +24,7 @@
         <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/pages.css" rel="stylesheet" type="text/css" />
         <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/menu.css" rel="stylesheet" type="text/css" />
         <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/responsive.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="{{URL::to('public/admin/ecommerce')}}/plugins/switchery/switchery.min.css">
+        <link rel="stylesheet" href="../plugins/switchery/switchery.min.css">
 
         <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -58,12 +58,12 @@
         <div id="wrapper">
 
             <!-- Top Bar Start -->
-            @include('admin/ecommerce/include/navbar')
+            @include('admin.ecommerce.include.navbar')
             <!-- Top Bar End -->
 
 
             <!-- ========== Left Sidebar Start ========== -->
-            @include('admin/ecommerce/include/left-sidebar')            
+            @include('admin.ecommerce.include.left-sidebar')
             <!-- Left Sidebar End -->
 
 
@@ -399,7 +399,7 @@
 
                 </div> <!-- content -->
 
-            @include('admin/ecommerce/include/footer')       
+                @include('admin.ecommerce.include.footer')
 
             </div>
 
@@ -410,7 +410,53 @@
 
 
             <!-- Right Sidebar -->
-            @include('admin/ecommerce/include/right-sidebar')            
+            <div class="side-bar right-bar">
+                <a href="javascript:void(0);" class="right-bar-toggle">
+                    <i class="mdi mdi-close-circle-outline"></i>
+                </a>
+                <h4 class="">Settings</h4>
+                <div class="setting-list nicescroll">
+                    <div class="row m-t-20">
+                        <div class="col-xs-8">
+                            <h5 class="m-0">Notifications</h5>
+                            <p class="text-muted m-b-0"><small>Do you need them?</small></p>
+                        </div>
+                        <div class="col-xs-4 text-right">
+                            <input type="checkbox" checked data-plugin="switchery" data-color="#7fc1fc" data-size="small"/>
+                        </div>
+                    </div>
+
+                    <div class="row m-t-20">
+                        <div class="col-xs-8">
+                            <h5 class="m-0">API Access</h5>
+                            <p class="m-b-0 text-muted"><small>Enable/Disable access</small></p>
+                        </div>
+                        <div class="col-xs-4 text-right">
+                            <input type="checkbox" checked data-plugin="switchery" data-color="#7fc1fc" data-size="small"/>
+                        </div>
+                    </div>
+
+                    <div class="row m-t-20">
+                        <div class="col-xs-8">
+                            <h5 class="m-0">Auto Updates</h5>
+                            <p class="m-b-0 text-muted"><small>Keep up to date</small></p>
+                        </div>
+                        <div class="col-xs-4 text-right">
+                            <input type="checkbox" checked data-plugin="switchery" data-color="#7fc1fc" data-size="small"/>
+                        </div>
+                    </div>
+
+                    <div class="row m-t-20">
+                        <div class="col-xs-8">
+                            <h5 class="m-0">Online Status</h5>
+                            <p class="m-b-0 text-muted"><small>Show your status to all</small></p>
+                        </div>
+                        <div class="col-xs-4 text-right">
+                            <input type="checkbox" checked data-plugin="switchery" data-color="#7fc1fc" data-size="small"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <!-- /Right-bar -->
 
         </div>
