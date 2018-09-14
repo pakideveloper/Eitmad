c105d27fbe348ed69e8e765cddcb6b60315c71e6
pakideveloper <ilovepakistan811@gmail.com>
2018-09-14 02:55:20 -0700

    Resolve brand create link

--- resources/views/admin/ecommerce/include/left-sidebar.blade.php
+++ resources/views/admin/ecommerce/include/left-sidebar.blade.php
@@ -37,7 +37,7 @@
                             <li class="has_sub">
                                 <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-invert-colors"></i> <span>Brand </span> <span class="menu-arrow"></span></a>
                                 <ul class="list-unstyled">
-                                    <li><a href="{{url('brand')}}/create">Create</a></li>
+                                    <li><a href="{{url('brands')}}/create">Create</a></li>
                                     <li><a href="ui-grid.html">View All</a></li>
                                 </ul>
                             </li>
