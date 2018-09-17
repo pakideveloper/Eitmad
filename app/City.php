d322cb509093a741c5cb39d28f5493fb9c1a7207
itfalcons <hassanzaidi20@gmail.com>
2018-09-17 17:56:30 +0500

    city model

--- app/City.php
+++ app/City.php
@@ -8,5 +8,12 @@ class City extends Model
 {
     protected $table = 'cities';
 
-
+    public function region()
+    {
+    	return $this->hasOne('App\Region','id', 'region_id');
+    } 
+    public function country()
+    {
+    	return $this->hasOne('App\country','id', 'country_id');
+    }  
 }
