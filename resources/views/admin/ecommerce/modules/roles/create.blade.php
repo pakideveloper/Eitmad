e1ccf4988983f3fc80641a8e5ae2ff74789494c4
bilalahmed99 <bilalahmed99@gmail.com>
2018-10-04 11:03:05 +0500

    Started roles

--- resources/views/admin/ecommerce/modules/roles/create.blade.php
+++ resources/views/admin/ecommerce/modules/roles/create.blade.php
@@ -11,21 +11,21 @@
         <!-- App favicon -->
         <link rel="shortcut icon" href="http://www.starmarketingonline.com/wp-content/uploads/2016/06/favicon.png">
         <!-- App title -->
-        <title>Eitmad</title>
+        <title>Create New Roles-ADMIN PANEL - EITMAD</title>
 
         <!-- Jquery filer css -->
-        <link href="{{URL::to('public/backendAssets')}}/plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
-        <link href="{{URL::to('public/backendAssets')}}/plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/plugins/jquery.filer/css/jquery.filer.css" rel="stylesheet" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/plugins/jquery.filer/css/themes/jquery.filer-dragdropbox-theme.css" rel="stylesheet" />
 
         <!-- App css -->
-        <link href="{{URL::to('public/backendAssets')}}/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="{{URL::to('public/backendAssets')}}/assets/css/core.css" rel="stylesheet" type="text/css" />
-        <link href="{{URL::to('public/backendAssets')}}/assets/css/components.css" rel="stylesheet" type="text/css" />
-        <link href="{{URL::to('public/backendAssets')}}/assets/css/icons.css" rel="stylesheet" type="text/css" />
-        <link href="{{URL::to('public/backendAssets')}}/assets/css/pages.css" rel="stylesheet" type="text/css" />
-        <link href="{{URL::to('public/backendAssets')}}/assets/css/menu.css" rel="stylesheet" type="text/css" />
-        <link href="{{URL::to('public/backendAssets')}}/assets/css/responsive.css" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" href="{{URL::to('public/backendAssets')}}/plugins/switchery/switchery.min.css">
+        <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="{{URL::to('public/admin/ecommerce')}}/assets/css/responsive.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="{{URL::to('public/admin/ecommerce')}}/plugins/switchery/switchery.min.css">
 
         <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -34,7 +34,7 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
 
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/modernizr.min.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/modernizr.min.js"></script>
 
     </head>
 
@@ -59,12 +59,12 @@
         <div id="wrapper">
 
             <!-- Top Bar Start -->
-            @include('backend/include/header')
+           @include('admin/ecommerce/include/navbar')
             <!-- Top Bar End -->
 
 
-            <!-- ========== Left Sidebar Start ========== -->
-            @include('backend/include/leftSidebar')
+            <!-- ========== Left Sidebar Start ======== == -->
+            @include('admin/ecommerce/include/left-Sidebar')
             <!-- Left Sidebar End -->
 
 
@@ -105,7 +105,7 @@
                                 <div class="card-box">
                                     <h4 class="header-title m-t-0">Create new role</h4>
 
-                                    <form action="{{URL::to('/admin/roles')}}" method="POST">
+                                    <form action="{{URL::to('ecommerce/admin/roles')}}" method="POST">
                                         {{csrf_field()}}
                                         <div class="row">
                                             <div class="col-md-6">
@@ -181,7 +181,7 @@
 
                 </div> <!-- content -->
 
-                @include('backend/include/footer')
+                @include('admin/ecommerce/include/footer')
 
             </div>
 
@@ -192,7 +192,7 @@
 
 
             <!-- Right Sidebar -->
-            @include('backend/include/rightSidebar')
+            @include('admin/ecommerce/include/right-Sidebar')
             <!-- /Right-bar -->
 
         </div>
@@ -205,28 +205,28 @@
         </script>
 
         <!-- jQuery  -->
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/jquery.min.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/bootstrap.min.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/detect.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/fastclick.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/jquery.blockUI.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/waves.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/jquery.slimscroll.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/jquery.scrollTo.min.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/plugins/switchery/switchery.min.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/jquery.min.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/bootstrap.min.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/detect.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/fastclick.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/jquery.blockUI.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/waves.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/jquery.slimscroll.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/jquery.scrollTo.min.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/plugins/switchery/switchery.min.js"></script>
 
         <!-- Jquery filer js -->
-        <script src="{{URL::to('public/backendAssets')}}/plugins/jquery.filer/js/jquery.filer.min.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/plugins/jquery.filer/js/jquery.filer.min.js"></script>
 
         <!-- page specific js -->
-        <script src="{{URL::to('public/backendAssets')}}/assets/pages/jquery.property-add.init.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/pages/jquery.property-add.init.js"></script>
 
         <!-- App js -->
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/jquery.core.js"></script>
-        <script src="{{URL::to('public/backendAssets')}}/assets/js/jquery.app.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/jquery.core.js"></script>
+        <script src="{{URL::to('public/admin/ecommerce')}}/assets/js/jquery.app.js"></script>
         <!-- sweet alert -->
          <script src="https://unpkg.com/sweetalert2@7.18.0/dist/sweetalert2.all.js"></script>
-        @include('sweetalert::alert')
+        
     </body>
 
 <!-- Mirrored from coderthemes.com/zircos/material-design/real-estate-add.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 08 Jun 2018 19:49:17 GMT -->
